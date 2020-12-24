45b1083c55b8f494c9ac05e1a85115de21fc9d58
mamochi31 <mamoruchikara9631@gmail.com>
2020-12-24 18:19:28 +0900

    性別コード作成機能実装

---
+++ app/controllers/sexes_controller.rb
@@ -0,0 +1,26 @@
+class SexesController < ApplicationController
+  def index
+    @sexes = Sex.all
+    @sex = Sex.new
+  end
+
+  def new
+  end
+
+  def create
+    # binding.pry
+    @sex = Sex.new(sex_params)
+    if @sex.valid?
+      @sex.save
+      redirect_to sexes_path
+    else
+      redirect_to sexes_path
+    end
+  end
+
+  private
+
+  def sex_params
+    params.require(:sex).permit(:code, :name)
+  end
+end
