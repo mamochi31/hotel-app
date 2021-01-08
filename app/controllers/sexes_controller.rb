1517bfec1e470989fb4ac502386736a46be76d09
mamochi31 <mamoruchikara9631@gmail.com>
2021-01-08 11:52:30 +0900

    性別コードの作成を検索機能へ変更

--- app/controllers/sexes_controller.rb
+++ app/controllers/sexes_controller.rb
@@ -1,7 +1,8 @@
 class SexesController < ApplicationController
+  before_action :search_sex, only: [:index, :search]
+  
   def index
     @sexes = Sex.all
-    @sex = Sex.new
   end
 
   def create
@@ -10,9 +11,18 @@ class SexesController < ApplicationController
     redirect_to sexes_path
   end
 
+  def search
+    @sexes = @q.result
+    render template: 'sexes/index'
+  end
+
   private
 
   def sex_params
     params.require(:sex).permit(:code, :name)
+  end
+
+  def search_sex
+    @q = Sex.ransack(params[:q])
   end
 end
