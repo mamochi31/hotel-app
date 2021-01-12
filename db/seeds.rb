bb93d8a70d2690c0d7a72c0dc153ad41d47712f5
mamochi31 <mamoruchikara9631@gmail.com>
2021-01-12 20:43:57 +0900

    seedファイル修正

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,5 @@
 # coding: utf-8
 
 10.times do
-  Gest.create(name1_kana: "サンプル", phone_number: "09012345678", room_type_id: 1, plan_id: 1, arr_date: "2021-03-24", dep_date: "2021-03-25", night: 1, adult: 1, number_of_room: 1, user_id: 1)
+  Gest.create(name1_kana: "サンプル", phone_number: "09012345678", room_type_id: 1, plan_id: 1, arr_date: "2021-03-24", dep_date: "2021-03-25", night: 1, adult: 1, child: 0, baby: 0, number_of_room: 1, user_id: 1)
 end
