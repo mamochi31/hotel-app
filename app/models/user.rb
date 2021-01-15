9cd7b3917c7623c3d6000f5f869607891adc6e33
mamochi31 <mamoruchikara9631@gmail.com>
2021-01-15 15:24:57 +0900

    パスワードのバリデーションを調整

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,9 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  validates :name, presence: true
+
+  PASSWORD_REGEX = /[a-z\d]{6,}/i
+  validates :password, format: { with: PASSWORD_REGEX }
 end
