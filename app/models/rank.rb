52a1a03e99cd5b71fa5ae27cedb014dc91d4b1dc
mamochi31 <mamoruchikara9631@gmail.com>
2021-01-20 11:59:48 +0900

    rank単体テストコードの記述

--- app/models/rank.rb
+++ app/models/rank.rb
@@ -1,7 +1,11 @@
 class Rank < ApplicationRecord
   has_many :gests
 
-  with_options presence: true, uniqueness: true do
+  with_options presence: true, uniqueness: { case_sensitive: true } do
     validates :code, :name
+  end
+
+  with_options format: { with: /\A[a-z0-9]+\z/ } do
+    validates :code
   end
 end
